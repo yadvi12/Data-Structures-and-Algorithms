d17ae352f6169c2ac22dacf56db4a084c5770ac3
Yadvi Bhalla <60046892+yadvi12@users.noreply.github.com>
2022-01-15 18:33:45 +0530

    Add files via upload

---
+++ Searching-Algos/Binary Search/binarySearch.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int binarySearch(int arr[], int n, int key)
+{
+    int start = 0;
+    int end = n - 1;
+
+    while (start <= end)
+    {
+        int mid = start + (end - start) / 2;
+        if (arr[mid] == key)
+        {
+            return mid;
+        }
+        else if (arr[mid] > key)
+        {
+            end = mid - 1;
+        }
+        else
+        {
+            start = mid + 1;
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    int key;
+    cin >> key;
+    int index = binarySearch(arr, n, key);
+    cout << index << endl;
+    return 0;
+}
