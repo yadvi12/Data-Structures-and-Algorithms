209ca95db6fc4d2b468bc37b4e5cad55e6177d4e
Yadvi Bhalla <60046892+yadvi12@users.noreply.github.com>
2022-01-16 18:18:34 +0530

    Update quickSort.cpp

--- Recursion/Day-06/quickSort.cpp
+++ Recursion/Day-06/quickSort.cpp
@@ -1,1 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int findPivot(int arr[], int start, int end)
+{
+    int pivot = arr[start];
+    int count = 0;
+    for (int i = start + 1; i <= end; i++)
+    {
+        if (arr[i] <= pivot)
+        {
+            count++;
+        }
+    }
+    int pivotIndex = start + count;
+    swap(arr[start], arr[pivotIndex]);
+
+    int i = start;
+    int j = end;
+    while (i < pivotIndex && j > pivotIndex)
+    {
+        while (arr[i] <= pivot)
+        {
+            i++;
+        }
+        while (arr[j] > pivot)
+        {
+            j--;
+        }
+        if (i < pivotIndex && j > pivotIndex)
+        {
+            swap(arr[i++], arr[j--]);
+        }
+    }
+    return pivotIndex;
+}
+
+void quickSort(int arr[], int start, int end)
+{
+    // BASE CASE
+    if (start >= end)
+    {
+        return;
+    }
+
+    // PROCESSING
+    int pivotIndex = findPivot(arr, start, end);
+
+    // RECURSIVE CALL
+    quickSort(arr, start, pivotIndex - 1);
+    quickSort(arr, pivotIndex + 1, end);
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    quickSort(arr, 0, n - 1);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
